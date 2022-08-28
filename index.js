256b5b8ed12bb1531ab54844d98d8c65ba261501
yPerrot <y.perrot@outlook.fr>
2022-08-28 23:45:55 +0200

    feat: Add Faile and Success dialog

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-const modal = document.querySelector("#modal");
-const closeModalBtn = document.querySelector("#close-modal-btn");
+const loadingDialog = document.querySelector("#loading-dialog");
+const failedDialog = document.querySelector("#failed-dialog");
+const successDialog = document.querySelector("#success-dialog");
+
+const closeModalBtn = document.querySelectorAll(".close-modal-btn");
 const form = document.querySelector('#form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   e.stopPropagation();
 
-  const link = form.elements['link'].value;
+  const gitLink = form.elements['link'].value;
 
-  modal.showModal();
+  fetch(`http://localhost:3000/download?link=${gitLink}`).then(async (response) => {
+    loadingDialog.close()
+    if (response.ok) {
+      successDialog.showModal();
+
+      const blobData = await response.blob()
+
+      var link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blobData);
+      link.download = gitLink.split('/').pop() + ".zip"
+      document.body.appendChild(link); // Required for FF
+      link.click();
+
+    } else {
+      failedDialog.showModal();
+    }
+  }).catch(err => {
+    failedDialog.showModal();
+    console.error(err);
+  })
+
+  loadingDialog.showModal();
 })
 
-closeModalBtn.addEventListener('click',() => modal.close())
+closeModalBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (loadingDialog.open) loadingDialog.close()
+    if (failedDialog.open) failedDialog.close()
+    if (successDialog.open) successDialog.close()
+  })
+})
+
