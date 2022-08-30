5fb20efcc329d5b2d99c92c348ad09e3c9520588
corentinleberre <corentin.leb29@gmail.com>
2022-08-30 22:31:03 +0200

    tech: :heavy_plus_sign: Add Vite and Prettier as dev dependencies

--- index.js
+++ index.js
@@ -3,44 +3,45 @@ const failedDialog = document.querySelector("#failed-dialog");
 const successDialog = document.querySelector("#success-dialog");
 
 const closeModalBtn = document.querySelectorAll(".close-modal-btn");
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	e.stopPropagation();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
 
-	const gitLink = form.elements['link'].value;
+  const gitLink = form.elements["link"].value;
 
-	fetch(`http://localhost:3000/download?link=${gitLink}`).then(async (response) => {
-		if (response.ok) {
-			successDialog.showModal();
+  fetch(`http://localhost:3000/download?link=${gitLink}`)
+    .then(async (response) => {
+      if (response.ok) {
+        successDialog.showModal();
 
-			const blobData = await response.blob();
+        const blobData = await response.blob();
 
-			const link = document.createElement("a");
-			link.href = window.URL.createObjectURL(blobData);
-			link.download = gitLink.split('/').pop() + ".zip"
-			document.body.appendChild(link); // Required for FF
-			link.click();
-		} else {
-			failedDialog.showModal();
-		}
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blobData);
+        link.download = gitLink.split("/").pop() + ".zip";
+        document.body.appendChild(link); // Required for FF
+        link.click();
+      } else {
+        failedDialog.showModal();
+      }
+    })
+    .catch((err) => {
+      failedDialog.showModal();
+      console.error(err);
+    })
+    .finally(() => {
+      loadingDialog.close();
+    });
 
-	}).catch(err => {
-		failedDialog.showModal();
-		console.error(err);
-	}).finally(() => {
-		loadingDialog.close();
-	})
+  loadingDialog.showModal();
+});
 
-	loadingDialog.showModal();
-})
-
-closeModalBtn.forEach(btn => {
-	btn.addEventListener('click', () => {
-		if (loadingDialog.open) loadingDialog.close()
-		if (failedDialog.open) failedDialog.close()
-		if (successDialog.open) successDialog.close()
-	})
-})
-
+closeModalBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (loadingDialog.open) loadingDialog.close();
+    if (failedDialog.open) failedDialog.close();
+    if (successDialog.open) successDialog.close();
+  });
+});
