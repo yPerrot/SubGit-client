ed905d6791fedcb36cfc2db6ddab1a3fe63436ce
yPerrot <y.perrot@outlook.fr>
2022-09-03 23:13:16 +0200

    style: Init ESLint

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
-const loadingDialog = document.querySelector("#loading-dialog");
-const failedDialog = document.querySelector("#failed-dialog");
-const successDialog = document.querySelector("#success-dialog");
+const loadingDialog = document.querySelector('#loading-dialog');
+const failedDialog = document.querySelector('#failed-dialog');
+const successDialog = document.querySelector('#success-dialog');
 
-const closeModalBtn = document.querySelectorAll(".close-modal-btn");
+const closeModalBtn = document.querySelectorAll('.close-modal-btn');
 const form = document.querySelector('#form');
 
 form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	e.stopPropagation();
+    e.preventDefault();
+    e.stopPropagation();
 
-	const gitURL = form.elements['url'].value;
+    const gitURL = form.elements.url.value;
 
-	fetch(`https://subgit-server.fly.dev/download?url=${gitURL}`).then(async (response) => {
-		if (response.ok) {
-			successDialog.showModal();
+    fetch(`https://subgit-server.fly.dev/download?url=${gitURL}`).then(async (response) => {
+        if (response.ok) {
+            successDialog.showModal();
 
-			const blobData = await response.blob();
+            const blobData = await response.blob();
 
-			const link = document.createElement("a");
-			link.href = window.URL.createObjectURL(blobData);
-			link.download = gitURL.split('/').pop() + ".zip"
-			document.body.appendChild(link); // Required for FF
-			link.click();
-		} else {
-			failedDialog.showModal();
-		}
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blobData);
+            link.download = `${gitURL.split('/').pop()}.zip`;
+            document.body.appendChild(link); // Required for FF
+            link.click();
+        } else {
+            failedDialog.showModal();
+        }
+    }).catch((err) => {
+        failedDialog.showModal();
+        console.error(err);
+    }).finally(() => {
+        loadingDialog.close();
+    });
 
-	}).catch(err => {
-		failedDialog.showModal();
-		console.error(err);
-	}).finally(() => {
-		loadingDialog.close();
-	})
+    loadingDialog.showModal();
+});
 
-	loadingDialog.showModal();
-})
-
-closeModalBtn.forEach(btn => {
-	btn.addEventListener('click', () => {
-		if (loadingDialog.open) loadingDialog.close()
-		if (failedDialog.open) failedDialog.close()
-		if (successDialog.open) successDialog.close()
-	})
-})
-
+closeModalBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (loadingDialog.open) loadingDialog.close();
+        if (failedDialog.open) failedDialog.close();
+        if (successDialog.open) successDialog.close();
+    });
+});
